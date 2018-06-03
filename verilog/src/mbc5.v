e34e199c1128cbd02560e3a5a40afd85bb9321ac
David Hurst <sideshowdave7@gmail.com>
2018-06-02 19:48:46 -0600

    working version

--- verilog/src/mbc5.v
+++ verilog/src/mbc5.v
@@ -2,7 +2,6 @@ module mbc5(
 	 //GB data and latch pins
 	 input [7:0] gb_data,
 	 input       gb_write_n,
-	 input       gb_read_n,
 
 	 //GB rst
 	 input       rst_n,
@@ -14,7 +13,7 @@ module mbc5(
 	 input       addr_15,
 	 input       addr_14,
 	 input       addr_13,
-   input       addr_12,
+    input       addr_12,
 
 	 //ROM Mapped Upper address bits
 	 output      m0,
@@ -22,17 +21,26 @@ module mbc5(
 	 output      m2,
 	 output      m3,
 	 output      m4,
+	 output      m5,
+	 output      m6,
+	 output      m7,
+	 output      m8,
 
 	 //Extended address bits
 	 output      ea0,
 	 output      ea1,
+	 output      ea2,
+	 output      ea3,
 
 	 //Chip selects
-	 output      ram_cs,
 	 output      ram_cs_n,
-	 output      rom_cs_n
+	 
+	 output      led,
+	 input       pb
     );
-   
+	
+	assign led = pb;
+	
    //MBC Logic
    //Ram enable & logic
    reg         RAM_enable;
@@ -52,18 +60,18 @@ module mbc5(
    //Rom bank write enable & logic
    reg [8:0] ROM_bank;
    //Lower 8 bits
-   assign ROM_bank_wr_en = {addr_15, addr_14, addr_13, addr_12} == 3'b0010 & ~gb_write_n;
-   always@(posedge ROM_bank_wr_en) begin
+   assign ROM_bank_lower_wr_en = {addr_15, addr_14, addr_13} == 3'b001 & ~gb_write_n;
+   always@(negedge ROM_bank_lower_wr_en) begin
       if (~rst_n) begin
-         ROM_bank <= 8'h00;
+         ROM_bank[7:0] <= 8'h00;
       end else begin
-         ROM_bank <= gb_data[7:0];
+         ROM_bank[7:0] <= gb_data[7:0];
       end
    end
 
    //Upper bit
-   assign ROM_bank_wr_en = {addr_15, addr_14, addr_13, addr_12} == 3'b0011 & ~gb_write_n;
-   always@(posedge ROM_bank_wr_en) begin
+   assign ROM_bank_upper_wr_en = {addr_15, addr_14, addr_13, addr_12} == 4'b0011 & ~gb_write_n;
+   always@(negedge ROM_bank_upper_wr_en) begin
 	    if (~rst_n) begin
 	       ROM_bank[8] <= 1'b0;
 	    end else begin
@@ -73,8 +81,8 @@ module mbc5(
 
    //Ram bank enable & logic
    reg [3:0] RAM_bank;
-   assign RAM_bank_wr_en = {addr_15, addr_14, addr_13} == 3'b010 & ~gb_write_n;
-   always@(posedge RAM_bank_wr_en) begin
+   assign RAM_bank_wr_en = {addr_15, addr_14} == 2'b01 & ~gb_write_n;
+   always@(negedge RAM_bank_wr_en) begin
 	    if (~rst_n) begin
 	       RAM_bank <= 4'h0;
 	    end else begin
@@ -85,7 +93,7 @@ module mbc5(
    //Mode select (either RAM or ROM mode)
    reg rom_mode;
    assign rom_mode_wr_en = {addr_15, addr_14, addr_13} == 3'b011 & ~gb_write_n;
-   always@(posedge rom_mode_wr_en) begin
+   always@(negedge rom_mode_wr_en) begin
       if (~rst_n) begin
 		     rom_mode <= 1'b0;
 	    end else begin
@@ -93,21 +101,21 @@ module mbc5(
 	    end
    end
 
-   assign m0 = ROM_bank[0];
-   assign m1 = ROM_bank[1];
-   assign m2 = ROM_bank[2];
-   assign m3 = ROM_bank[3];
-   assign m4 = ROM_bank[4];
-   assign m5 = ROM_bank[5];
-   assign m6 = ROM_bank[6];
-   assign m7 = ROM_bank[7];
-   assign m8 = ROM_bank[8];
+   assign m0 = ROM_bank[0] & addr_14;
+   assign m1 = ROM_bank[1] & addr_14;
+   assign m2 = ROM_bank[2] & addr_14;
+   assign m3 = ROM_bank[3] & addr_14;
+   assign m4 = ROM_bank[4] & addr_14;
+   assign m5 = ROM_bank[5] & addr_14;
+   assign m6 = ROM_bank[6] & addr_14;
+   assign m7 = ROM_bank[7] & addr_14;
+   assign m8 = ROM_bank[8] & addr_14;
 
-   assign ea0 = (~rom_mode & ~addr_14) ? 1'b0: RAM_bank[0];
-   assign ea1 = (~rom_mode & ~addr_14) ? 1'b0: RAM_bank[1];
+   assign ea0 = RAM_bank[0];
+   assign ea1 = RAM_bank[1];
+	assign ea2 = RAM_bank[2];
+	assign ea3 = RAM_bank[3];
 
-   assign ram_cs_n = ~ram_cs;
-   assign ram_cs = ~cs_n & ~addr_14 & RAM_enable;
-   assign rom_cs_n = ~((~addr_15 & ~gb_read_n) | ~rst_n);
+   assign ram_cs_n = ~RAM_enable | cs_n;
 
 endmodule
